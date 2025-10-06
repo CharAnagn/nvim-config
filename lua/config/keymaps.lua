19b827c932d3a129c6bef6475e07b4a17dda600f
Babis Anagnostou <cganagnostou@gmail.com>
2025-10-06 18:52:32 +0200

    keymaps: center on pageup/down and seaching

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,21 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- Shorthand for setting keymaps
+local map = vim.keymap.set
+local opts = { noremap = true, silent = true }
+
+-- Center cursor when moving half-page up/down
+map("n", "<C-d>", "<C-d>zz", opts)
+map("n", "<C-u>", "<C-u>zz", opts)
+
+-- Center cursor when navigating search results
+map("n", "n", "nzzzv", opts)
+map("n", "N", "Nzzzv", opts)
+
+-- Example: clear search highlights
+map("n", "<leader>h", ":nohlsearch<CR>", opts)
+
+-- Example: quick save
+map("n", "<leader>w", ":w<CR>", opts)
